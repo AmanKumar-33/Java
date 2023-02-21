dfe5689c71c23b410def8b7052adaca5236dd366
Aman <89511864+Starliad@users.noreply.github.com>
2023-02-21 23:21:46 +0530

    Add files via upload

---
+++ A.java
@@ -0,0 +1,8 @@
+package Pack1;
+
+
+public class A{
+   public void msg(){
+    System.out.println("this message is for aman singh");
+    }
+}
---
+++ Aman.java
@@ -0,0 +1,8 @@
+package Pack1;
+
+
+public class Aman{
+   public void msg(){
+    System.out.println("this message is for aman singh");
+    }
+}
