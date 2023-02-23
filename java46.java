7046890f26f8c0f26989be3eab57a98ba69ae560
Aman <89511864+Starliad@users.noreply.github.com>
2023-02-23 11:38:12 +0530

    Add files via upload

---
+++ java46.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class Java46{
+  public static void main(String args[]){
+  
+  system.out.print("enter the radius of circle");
+  Scanner sc = new Scanner (System.in);
+  double radius = sc.nextDouble();
+  double area = 3.14 * radius *radius;
+  System.out.println("area is " +area);
+  }
+  }
+
